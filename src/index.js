28d8b5bbfdcd9d66aae80a35f12199a4bb1e6b5c
@Singh_Peeyush <43371031+Peeyush-kumar7656@users.noreply.github.com>
2021-05-12 20:50:11 +0530

    modify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
-
-
+ReactDOM.render(<App />,document.getElementById("root"));
+// function App(){
+//     return(
+//         <div id ="main">
+//             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+//         </div>
+//     );}
+// export default App;
